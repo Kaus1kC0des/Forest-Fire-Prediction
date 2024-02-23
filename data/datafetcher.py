8cfb2c48d5ccf1b71e2aedd18cd7d78fc4a60176
Kaus1kC0des <kausikdevanathan@gmail.com>
2024-02-23 18:48:02 +0530

    ML Modelling and Datafetcher files

--- data/datafetcher.py
+++ data/datafetcher.py
@@ -1,5 +1,11 @@
 import requests
+import csv
 
-data = requests.get("https://firms.modaps.eosdis.nasa.gov/api/country/csv/0f2d0c92011b4de12d669b621fcc535c/VIIRS_NOAA20_NRT/IND/1/2024-02-01")
 
-print(data.text)
+for i in range(1,32):
+    if i<10:
+        data = requests.get("https://firms.modaps.eosdis.nasa.gov/api/country/csv/0f2d0c92011b4de12d669b621fcc535c/VIIRS_NOAA20_NRT/IND/1/2024-01-0{i}")
+    else:
+        data = requests.get(
+            "https://firms.modaps.eosdis.nasa.gov/api/country/csv/0f2d0c92011b4de12d669b621fcc535c/VIIRS_NOAA20_NRT/IND/1/2024-01-{i}")
+    # Save it into a pandas dataframe
