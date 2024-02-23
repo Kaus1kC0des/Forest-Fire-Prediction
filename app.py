a8e12b40718c7e925ac22e145a3e7187faa81b18
Karthick Sriram <karthicksriram71@gmail.com>
2024-02-24 03:58:04 +0530

    back to flask done, waiting for front to react

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template,jsonify
+from flask import Flask, render_template,jsonify,request
+
 import pickle
 import bz2
 import requests
@@ -14,21 +15,32 @@ def homePage():
     return render_template('index.html')
 @app.route('/predict.html')
 def predict():
+    
     return render_template('predict.html')
 
 
 R_pickle_in = bz2.BZ2File('./regression.pkl', 'rb')
 model= pickle.load(R_pickle_in)
     
-@app.route('/api/predict', methods=['GET','POST'])
+@app.route('/clicked', methods=['GET'])
+def clicked():
+    data = request.args.get('title')
+    print(data)
+    # Process the data as needed
+    # Return the processed data
+
+    return data
+@app.route('/api/predict/', methods=['GET','POST'])
 def predict_api():
-   
-    weather=(requests.get(f'http://api.weatherapi.com/v1/current.json?key=302ef7ffae9443af9b7171520241902&q={lat},{lon}&aqi=no'))
+    location = request.args.get('location')
+    weather=(requests.get(f'http://api.weatherapi.com/v1/current.json?key=302ef7ffae9443af9b7171520241902&q={location}&aqi=no'))
     # modis=modis.split("\n")[1].split(",")
     # l=[modis[1],modis[2],modis[6].split("-")[1],modis[3],modis[4],modis[7],modis[12],modis[14]]
     # l = np.array(l)
-    data = weather.get_json()
-    l=[35,10,44,47,2]
+    data = weather.json()
+    print(data)
+    print(location)
+    l=[data['current']['temp_c'],data["current"]["wind_kph"],35,30,2]
     l = [np.array(l)]
     # data=s.transform(l)
 
@@ -37,7 +49,7 @@ def predict_api():
     predictions = model.predict(l)
     # # Return the predictions as a JSON response
     print(predictions)
-    return "-1"
+    return render_template('predict.html',prediction_text=f'Predicted AQI: {predictions[0]}')
 
 
 if __name__ == '__main__':
