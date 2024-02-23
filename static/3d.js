e47ea147bfc966c7e35afac4163eb876ab5cc881
Karthick Sriram <karthicksriram71@gmail.com>
2024-02-23 14:12:33 +0530

    added img map

--- static/3d.js
+++ static/3d.js
@@ -8,8 +8,12 @@ var loader = new STLLoader();
 var scene = new THREE.Scene();
 
 // Create a camera
-var camera = new THREE.PerspectiveCamera(270, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(100, 20 , 30 ); // Update camera position
+var camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(0, 0 , 5 ); // Update camera position
+camera.rotation.x = 0; // Set rotation to 0.5 radians on the x-axis
+camera.rotation.y = 90; // Set rotation to 0.5 radians on the y-axis
+camera.rotation.z = 0; // Set rotation to 0.5 radians on the z-axis
+
 camera.lookAt(scene.position); // Look at the center of the scene
 
 // Create a renderer
@@ -21,22 +25,26 @@ document.body.appendChild(renderer.domElement);
 loader.load('../final_map.stl', function (geometry) {
   var material = new THREE.MeshNormalMaterial();
   var mesh = new THREE.Mesh(geometry, material);
+
+  
+
   // Scale the model
   // mesh.scale.set(30, 30, 30);
-  
+
   // Center the model
-  geometry.computeBoundingBox();
-  var boundingBox = geometry.boundingBox;
-  var center = new THREE.Vector3();
-  boundingBox.getCenter(center);
-  mesh.position.sub(center);
-  
+  // geometry.computeBoundingBox();
+  // var boundingBox = geometry.boundingBox;
+  // var center = new THREE.Vector3();
+  // boundingBox.getCenter(center);
+  // mesh.position.set(24.600, -6.060, 16.920);
+   // Set the new position of the mesh
+  mesh.position.set(0, 0, 0);
   scene.add(mesh);
 
   // Animate the model
   function animate() {
     requestAnimationFrame(animate);
-    mesh.rotation.x = 1; // Set rotation to 0 on the x-axis
+    mesh.rotation.x  = 1; // Set rotation to 0 on the x-axis
     mesh.rotation.y = 1; // Set rotation to 0 on the y-axis
     mesh.rotation.z = 1; // Set rotation to 0 on the z-axis
     renderer.render(scene, camera); // Render the scene with the updated camera position and rotation
@@ -44,4 +52,4 @@ loader.load('../final_map.stl', function (geometry) {
 
   animate();
 
-});
+  });
--- static/dashboard.jsx
+++ static/dashboard.jsx
@@ -1,38 +1,57 @@
-// dashboard.js
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
-import './dash.css';
-const Dashboard = () => {
-  const [temperature, setTemperature] = useState(0);
-  const [humidity, setHumidity] = useState(0);
-  const [tuff, setTuff] = useState(0);
-  const [fuelLevel, setFuelLevel] = useState(0);
+class Dashboard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      forest_name: "__________",
+      content: "_____",
+      temperature: 25,
+      humidity: 60,
+      duffPercentage: 80,
+      showContent: true,
+    };
+  }
 
-  // Function to simulate updating data
-  const updateData = () => {
-    setTemperature(Math.floor(Math.random() * 100));
-    setHumidity(Math.floor(Math.random() * 100));
-    setTuff(Math.floor(Math.random() * 100));
-    setFuelLevel(Math.floor(Math.random() * 100));
+  toggleContent = () => {
+    this.setState((prevState) => ({
+      showContent: !prevState.showContent,
+    }));
   };
 
-  return (
-    <div className="dashboard">
-      <div className="data-container">
-        <h2>Temperature: {temperature}°C</h2>
+  render() {
+    return (
+      <div className="container">
+        {/* {this.state.showContent && ( */}
+          <div className="content">
+            <div className="name">FOREST NAME:{this.state.forest_name}</div>
+            <div className="images">
+              <img src="./icon/fire.png" />
+              {/* <img src="./icon/heating.png" />
+              <img src="./icon/sun.png" /> */}
+            </div>
+            <div className="status"><p>Fire Danger Status:{this.state.content}</p></div>
+            <div className="card">
+              <div className="label"><i className="fas fa-thermometer-half"></i> Temperature:</div>
+              <div>{this.state.temperature} °C</div>
+            </div>
+            <div className="card">
+              <div className="label"><i className="fas fa-tint"></i> Humidity:</div>
+              <div>{this.state.humidity} %</div>
+            </div>
+            <div className="card">
+              <div className="label"><i className="fas fa-fire"></i> Duff Percentage:</div>
+              <div>{this.state.duffPercentage} %</div>
+            </div>
+          </div>
+        {/* )}
+        <button className="button" onClick={this.toggleContent}>
+          {this.state.showContent ? "Hide Content" : "Show Content"}
+        </button> */}
       </div>
-      <div className="data-container">
-        <h2>Humidity: {humidity}%</h2>
-      </div>
-      <div className="data-container">
-        <h2>Tuff: {tuff}</h2>
-      </div>
-      <div className="data-container">
-        <h2>Fuel Level: {fuelLevel}%</h2>
-      </div>
-      <button onClick={updateData}>Update Data</button>
-    </div>
-  );
-};
+    );
+  }
+}
 
-ReactDOM.render(<Dashboard />, document.getElementById('dashboard'));
+ReactDOM.render(<Dashboard />, document.getElementById("root"));
+
+
+
